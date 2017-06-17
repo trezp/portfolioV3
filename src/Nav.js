fdf307715f399dc59a522f64ca48c928a67bf75a
Treasure Porth <treasure.porth@teamtreehouse.com>
2017-06-17 15:19:34 -0700

    Hid API key

--- src/Nav.js
+++ src/Nav.js
@@ -1,1 +1,3 @@
 import React from 'react';
+import { Route, NavLink, Redirect } from 'react-router-dom'
+import PhotosContainer from './PhotosContainer';
--- src/Photos.js
+++ src/Photos.js
@@ -4,14 +4,14 @@ import axios from 'axios';
 class Photos extends React.Component {
   constructor(props){
     super(props);
-
+    console.log(this.props.api_key)
     this.state = {
       photos: []
     };
   }
 
   componentDidMount(){
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b1c6f4e7e1af551e58b30f871a49c3d4&tags=${this.props.photo}&per_page=25&format=json&nojsoncallback=1`)
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.props.api_key}&tags=${this.props.photo}&per_page=25&format=json&nojsoncallback=1`)
     .then((response) => {
       console.log(response)
       const photos = response.data.photos.photo.map( photo => {
