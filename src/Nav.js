55d96a4ca0c985f76891e88ff48f2dd5ac043208
Treasure Porth <treasure.porth@teamtreehouse.com>
2017-06-19 20:59:46 -0700

    Basic styles

--- src/Nav.js
+++ src/Nav.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Nav = ()=> (
-    <header>
+    <nav className="main-nav">
       <ul>
           <li><NavLink to='/cats'>Cats</NavLink></li>
           <li><NavLink to='/flowers'>Flowers</NavLink></li>
           <li><NavLink to='/rainbows'>Rainbows</NavLink></li>
-          <li><NavLink to='/kittens'>Kittens</NavLink></li>
           <li><NavLink to='/cake'>Cake</NavLink></li>
       </ul>
-    </header>
+    </nav>
 )
 
 export default Nav;
