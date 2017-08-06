8df54b1f3017baa4d483c77d3e04a383617d00ec
Treasure Porth <treasure.porth@teamtreehouse.com>
2017-08-06 00:08:51 -0700

    Basic scaffolding, refactored portfolio section

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,28 +1,65 @@
 import React from 'react';
 import { Link, BrowserRouter, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Portfolio = () =>(
-	<BrowserRouter>
-		<div>
-			<h2>Portfolio</h2>
-			<p>This is my portfolio.</p>
-			 <nav className="main-nav">
-				 <ul>
-				  	<li><Link to='/portfolio/professional'>Professional</Link></li>
-				  	<li><Link to='/portfolio/smaller'>Smaller</Link></li>
-				    <li><Link to='/portfolio/inprogress'>In Progress</Link></li>
-				 </ul>
-				 <Route
-				    	path="/portfolio/:section"
-				    	component={Section}/>
-    		</nav>
-		</div>
-	</BrowserRouter>
-)
 
-const Section = ({ match }) => (
-  <div>
-    <h3>{match.params.section}</h3>
-  </div>
-)
+function SelectOption(props){
+	const sectionOptions = ['professional', 'smaller', 'inprogress', 'other']
+
+	return (
+		<nav className="main-nav">
+			<ul>
+		 	{sectionOptions.map(function(option){
+			 		return (
+			 			<li 
+			 				style={option===props.selectedOption ? {color: 'blue'} : null}
+			 				key={option}
+			 				onClick={props.onSelect.bind(null, option)}>
+			 				{option}
+			 			</li>
+			 		)
+		 		})}
+			</ul>
+		</nav>
+	)
+}
+
+SelectOption.propTypes = {
+	selectedOption: PropTypes.string.isRequired,
+	onSelect: PropTypes.func.isRequired
+}
+
+class Portfolio extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			selectedOption : "professional"
+		};
+		this.updateSection = this.updateSection.bind(this);
+	}
+
+	updateSection(option){
+		this.setState(function(){
+			return {
+				selectedOption: option
+			}
+		});
+	}
+	render(){
+		
+		return (
+ 			<BrowserRouter>
+				<div>
+					<h2>Portfolio</h2>
+					<p>This is my portfolio.</p>
+					<SelectOption
+						selectedOption={this.state.selectedOption}
+						onSelect={this.updateSection}/>
+				</div>
+			</BrowserRouter>
+		)
+		
+	}
+}
+
 export default Portfolio;
